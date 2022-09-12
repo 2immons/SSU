e392b59b5c7b90c2727e35ee10931651b596418f
Pavel <podoprigoraps@gmail.com>
2022-09-12 20:48:57 +0400

    up

---
+++ Graph/Graph1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph1
+{
+    public class Program
+    {
+        // В входном файле указывается количество вершин графа/орграфа и матрица смежности:
+        // Для заданного графа: 
+        // 3. удалить из графа ребро, соединяющее вершины а и b
+
+
+
+    }
+}
